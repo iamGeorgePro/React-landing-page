079bbfb1b72aafda1a0fe0cc3516a2db30a5dfa8
Arthur Emmanuel <yhawnoworries@gmail.com>
2022-12-04 08:52:23 +0000

    update

--- src/components/pages/Ui/DeveloperInfo.jsx
+++ src/components/pages/Ui/DeveloperInfo.jsx
@@ -10,7 +10,7 @@ function DeveloperInfo() {
     <div className='img1'>
         <img src={img1}  alt=''/>
     </div>
-    <div className='container  flex items-center justify-between'>
+    <div className='container block  md:flex items-center justify-between'>
     <div className='my-10 '>
       <p className= 'text-center bg-[#EBB40A] py-[11.5px] px-[35px] rounded-[31px] w-[260px] h-[46px] text-[20px] font-bold'>For developers</p>
       <div className='flex mt-[52px] gap-[14px]'>
@@ -22,24 +22,30 @@ function DeveloperInfo() {
       <div className='flex mt-[52px] gap-[14px]'>
         <div><img src={bar2}  alt=''/></div>
         <div>
-          <p className='text-[24px] leading-[30.72px] tracking-[2px] w-[350px] h-[30px] md:w-[569px]  md:h-[62px]'>Obtain and practice tech skills by solving real-world problems.</p>
+          <p className='text-[24px] leading-[30.72px] tracking-[2px] w-[350px] h-[30px] md:w-[569px]  md:h-[62px]'>Share expertise, experience and ideas with others..</p>
         </div>
       </div>
       <div className='flex mt-[52px] gap-[14px]'>
         <div><img src={bar}  alt=''/></div>
         <div>
-          <p className='text-[24px] leading-[30.72px] tracking-[2px] w-[350px] h-[30px] md:w-[569px]  md:h-[62px]'>Obtain and practice tech skills by solving real-world problems.</p>
+          <p className='text-[24px] leading-[30.72px] tracking-[2px] w-[350px] h-[30px] md:w-[569px]  md:h-[62px]'>Learn how to collaborate with teammates under pressure.</p>
         </div>
       </div>
       <div className='flex mt-[52px] gap-[14px]'>
         <div><img src={bar2}  alt=''/></div>
         <div>
-          <p className='text-[24px] leading-[30.72px] tracking-[2px] w-[350px] h-[30px] md:w-[569px]  md:h-[62px]'>Obtain and practice tech skills by solving real-world problems.</p>
+          <p className='text-[24px] leading-[30.72px] tracking-[2px] w-[350px] h-[30px] md:w-[569px]  md:h-[62px]'>Potentially get job offers from top-tier enterprises.</p>
+        </div>
+      </div>
+      <div className='flex mt-[52px] gap-[14px]'>
+        <div><img src={bar2}  alt=''/></div>
+        <div>
+        <p className='text-[24px] leading-[30.72px] tracking-[2px] w-[350px] h-[30px] md:w-[569px]  md:h-[62px]'>Get continous funding or grant for your project/idea.</p>
         </div>
       </div>
     </div>
     <div className=''>
-      <img className='mt-10' src={img2}  alt=''/>
+      <img className='mt-32' src={img2}  alt=''/>
     </div>
     </div>
    </section>
