b2b53d8c9abe4188d9a11d33fb955a76b948cb00
Arthur Emmanuel <yhawnoworries@gmail.com>
2022-12-08 08:35:05 +0000

    update

--- src/components/pages/Ui/Footer.jsx
+++ src/components/pages/Ui/Footer.jsx
@@ -7,8 +7,8 @@ function Footer() {
         <p className='mx-auto leading-[40px]  md:leading-[52.2px] tracking-[1.3px] w-[350px]  md:w-[913px] h-[95px]  md:h-[149px] text-[25px]  md:text-[40px] font-bold'>Discover the world of innovation at  <span className=' text-yellow-500'>Kwabees</span>, BUIDLs,
          continuous funding, grants, bounties, and hackathons.</p>
         </div>
-      <div className='mx-auto  text-center mt-28'>
-      <button className='btn py-[15px] px-[31px]  text-[#065289] rounded-[10px] mt-[65px]'>Join the wait list</button>
+      <div className='mx-auto  text-center  mt-28 md:mt-[65px]'>
+      <button className='btn py-[15px] px-[31px]  text-[#065289] rounded-[10px]'>Join the wait list</button>
       </div>
        
     </section>
