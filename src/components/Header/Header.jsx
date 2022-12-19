a8f37732a380dee096a7f85353ef5aeba50ce398
Arthur Emmanuel <yhawnoworries@gmail.com>
2022-12-19 07:55:11 +0000

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,35 +1,35 @@
-import React from 'react'
-import logo from '../../assets/logo/logo.png'
-import img1 from '../../assets/img/discord-fill (1).png'
-import img2 from '../../assets/img/telegram-fill (1).png'
-import img3 from '../../assets/img/twitter-fill (1).png'
+// import React from 'react'
+// import logo from '../../assets/logo/logo.png'
+// import img1 from '../../assets/img/discord-fill (1).png'
+// import img2 from '../../assets/img/telegram-fill (1).png'
+// import img3 from '../../assets/img/twitter-fill (1).png'
 
-function Header() {
-  return (
-    <>
-    <section className='bg-[#FFFFFF9C] h-[82px]'>
-    <div className='container '>
-        <div className='flex justify-between items-center'>
-            <div>
-              <img src={logo} alt='' />
-            </div>
-            <div className='flex gap-[22px] cursor-pointer'>
-          <a target='_blank'  rel="noreferrer" href='https://twitter.com/Kwabeeshub'>
-            <img className='hover:opacity-5 social' src={img3}  alt=''/>
-            </a>
-            <a  rel="noreferrer" target='_blank' href='https://discord.gg/EYBtvT9HTB'>
-            <img className='hover:opacity-5 social' src={img1}  alt=''/>
-            </a>
-            <a target='_blank' href='#'>
-            <img className='hover:opacity-5 social' src={img2}  alt=''/>
-            </a>
-            </div>
-        </div>
+// function Header() {
+//   return (
+//     <>
+//     <section className='bg-[#FFFFFF9C] h-[82px]'>
+//     <div className='container '>
+//         <div className='flex justify-between items-center'>
+//             <div>
+//               <img src={logo} alt='' />
+//             </div>
+//             <div className='flex gap-[22px] cursor-pointer'>
+//           <a target='_blank'  rel="noreferrer" href='https://twitter.com/Kwabeeshub'>
+//             <img className='hover:opacity-5 social' src={img3}  alt=''/>
+//             </a>
+//             <a  rel="noreferrer" target='_blank' href='https://discord.gg/EYBtvT9HTB'>
+//             <img className='hover:opacity-5 social' src={img1}  alt=''/>
+//             </a>
+//             <a target='_blank' href='#'>
+//             <img className='hover:opacity-5 social' src={img2}  alt=''/>
+//             </a>
+//             </div>
+//         </div>
 
-    </div>
-    </section>
-    </>
-  )
-}
+//     </div>
+//     </section>
+//     </>
+//   )
+// }
 
-export default Header
+// export default Header
